5593e82096a64e3e1146e51c99043c883530daec
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2024-09-05 13:31:40 -0600

    end of exercise one

--- EXERCISE_ONE/EXERCISE_ONE/EXERCISE_ONE.cpp
+++ EXERCISE_ONE/EXERCISE_ONE/EXERCISE_ONE.cpp
@@ -44,7 +44,48 @@ void printPath() {
     }
     std::cout << std::endl;
 }
+bool checkMap(Map& map, BooleanMap& booleanMap, int startX, int startY, int endX, int endY) {
+    if (map.map[startX][startY] == 0 || map.map[endX][endY] == 0) {
+        return false; 
+    }
+    ValidCell* localStack = nullptr;
+    push(new ValidCell{ startX, startY, nullptr });
+    booleanMap.visited[startX][startY] = true;
 
+    while (pathStack != nullptr) {
+        ValidCell* current = pop();
+        int x = current->positionX;
+        int y = current->positionY;
+
+        if (x == endX && y == endY) {
+            push(new ValidCell{ x, y, nullptr });
+            return true;
+        }
+        if (x > 0 && map.map[x - 1][y] == 1 && !booleanMap.visited[x - 1][y]) {
+            booleanMap.visited[x - 1][y] = true;
+            push(new ValidCell{ x - 1, y, nullptr });
+        }
+        if (x < 6 && map.map[x + 1][y] == 1 && !booleanMap.visited[x + 1][y]) {
+            booleanMap.visited[x + 1][y] = true;
+            push(new ValidCell{ x + 1, y, nullptr });
+        }
+        if (y > 0 && map.map[x][y - 1] == 1 && !booleanMap.visited[x][y - 1]) {
+            booleanMap.visited[x][y - 1] = true;
+            push(new ValidCell{ x, y - 1, nullptr });
+        }
+        if (y < 6 && map.map[x][y + 1] == 1 && !booleanMap.visited[x][y + 1]) {
+            booleanMap.visited[x][y + 1] = true;
+            push(new ValidCell{ x, y + 1, nullptr });
+        }
+    }
+
+    while (pathStack != nullptr) {
+        ValidCell* temp = pop();
+        delete temp;
+    }
+
+    return false;
+}
 bool explore(Map& map, BooleanMap& booleanMap, int x, int y, int endX, int endY) {
     if (x == endX && y == endY) {
         push(new ValidCell{ x, y, nullptr });
@@ -66,14 +107,6 @@ bool explore(Map& map, BooleanMap& booleanMap, int x, int y, int endX, int endY)
     pop();
     return false;
 }
-
-bool checkMap(Map& map, BooleanMap& booleanMap, int startX, int startY, int endX, int endY) {
-    if (map.map[startX][startY] == 0 || map.map[endX][endY] == 0) {
-        return false; 
-    }
-
-    return explore(map, booleanMap, startX, startY, endX, endY);
-}
 void printMaze(Map map) {
     for (int i = 0; i < 7; i++) {{
             for (int j = 0; j < 7; j++) {
@@ -84,31 +117,54 @@ void printMaze(Map map) {
     }
 }
 int main() {
-    Map map;
-    BooleanMap booleanMap;
-
-    printMaze(map);
-
     
-    int startX, startY, endX, endY;
-    std::cout << "Ingrese la posicion inicial (x y): ";
-    std::cin >> startX >> startY;
-    std::cout << "Ingrese la posicion final (x y): ";
-    std::cin >> endX >> endY;
-    if (startX < 0 || startX >= 7 || startY < 0 || startY >= 7 ||
-        endX < 0 || endX >= 7 || endY < 0 || endY >= 7) {
-        std::cerr << "Las coordenadas estan fuera del rango valido." << std::endl;
-        return 1;
-    }
+    char choice;
+    do {
+        Map map;
+        BooleanMap booleanMap;
+        printMaze(map);
+        int startX, startY, endX, endY;
+        std::cout << "Ingrese la posicion inicial (x y): ";
+        std::cin >> startX >> startY;
+        std::cout << "Ingrese la posicion final (x y): ";
+        std::cin >> endX >> endY;
 
-    if (checkMap(map, booleanMap, startX, startY, endX, endY)) {
-        std::cout << "Existe un camino valido desde (" << startX << "," << startY << ") hasta (" << endX << "," << endY << ")." << std::endl;
-        printPath();
-    }
-    else {
-        std::cout << "No existe un camino valido." << std::endl;
-    } 
+        if (startX < 0 || startX >= 7 || startY < 0 || startY >= 7 ||
+            endX < 0 || endX >= 7 || endY < 0 || endY >= 7) {
+            std::cerr << "Las coordenadas estan fuera del rango valido." << std::endl;
+            continue;
+        }
+        std::cout << "Elija el metodo de busqueda:\n";
+        std::cout << "1. Busqueda recursiva\n";
+        std::cout << "2. Busqueda iterativa\n";
+        std::cout << "0. Salir\n";
+        std::cin >> choice;
 
+        switch (choice) {
+        case '1':
+            if (explore(map, booleanMap, startX, startY, endX, endY)) {
+                std::cout << "Existe un camino valido desde (" << startX << "," << startY << ") hasta (" << endX << "," << endY << ")." << std::endl;
+                printPath();
+            }
+            else {
+                std::cout << "No existe un camino valido." << std::endl;
+            }
+            break;
+        case '2':
+            if (checkMap(map, booleanMap, startX, startY, endX, endY)) {
+                std::cout << "Existe un camino valido desde (" << startX << "," << startY << ") hasta (" << endX << "," << endY << ")." << std::endl;
+                printPath();
+            }
+            else {
+                std::cout << "No existe un camino valido." << std::endl;
+            }
+            break;
+        case '0':
+            std::cout << "Saliendo del programa." << std::endl;
+            continue;
+        default:
+            std::cerr << "Opcion no valida." << std::endl;
+        }
 
-    return 0;
+    } while (choice != '0');
 }
